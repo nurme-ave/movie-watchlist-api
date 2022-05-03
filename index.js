a5e8327d27a99af89f6d4ee570bcd809f23b1e38
Ave Nurme <nurme.ave@gmail.com>
2022-05-03 22:51:15 +0300

    add type parameter back in the URL

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function getMovieId(input) {
   const arrMoviesId = [];
   try {
     const response = await fetch(
-      `https://www.omdbapi.com/?s=${input}&plot=short&apikey=f7c0d604`
+      `https://www.omdbapi.com/?s=${input}&plot=short&type=movie&apikey=f7c0d604`
     );
     const moviesData = await response.json();
     const movies = moviesData.Search;
@@ -35,7 +35,7 @@ async function getMovieDetails(arr) {
   try {
     for (let item of arr) {
       const res = await fetch(
-        `https://www.omdbapi.com/?i=${item}&plot=short&apikey=f7c0d604`
+        `https://www.omdbapi.com/?i=${item}&plot=short&type=movie&apikey=f7c0d604`
       );
       movieDetails = await res.json();
       arrmovieDetails.push(movieDetails);
