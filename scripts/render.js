f26e2c0bbf9067df776107d5d70e32dda67ab1d1
Ave Nurme <nurme.ave@gmail.com>
2022-05-10 01:08:36 +0300

    deconstruct 'detail' for cleaner code in render.js

--- scripts/render.js
+++ scripts/render.js
@@ -2,14 +2,15 @@ function renderMovieDetails(details, sign='plus') {
   let detailsHtml = '';
   detailsHtml += details
     .map((detail) => {
+      const { Poster, Title, imdbRating, Runtime, Genre, imdbID, Plot } = detail;
       return `
     <div id="contentDiv" class="contentDiv">
-      <img src='${detail.Poster}'>
+      <img src='${Poster}'>
       <div class=contentP>
-        <p class="title">${detail.Title} <span class="rating">⭐ ${detail.imdbRating}</span></p>
-        <p class="runtime">${detail.Runtime} <span class="genre">${detail.Genre}</span>
-          <button id="${detail.imdbID}" class="activity-button"><i class="fa-solid fa-circle-${sign}"></i> Watchlist</button></p>
-        <p class="plot">${detail.Plot}</p>
+        <p class="title">${Title} <span class="rating">⭐ ${imdbRating}</span></p>
+        <p class="runtime">${Runtime} <span class="genre">${Genre}</span>
+          <button id="${imdbID}" class="activity-button"><i class="fa-solid fa-circle-${sign}"></i> Watchlist</button></p>
+        <p class="plot">${Plot}</p>
       </div>
     </div>
     `;
